42de4e409c789caefc03b027e218cf2b5d841c3f
PlugsBot <miniapp47@outlook.com>
2026-06-03 23:21:13 +0200

    new

--- script.js
+++ script.js
@@ -943,7 +943,7 @@ function renderProductListSimple(categoryId) {
         // --- 3. INJECTION (On cache la galerie si pas de média) ---
         detailsContainer.innerHTML = `
             ${hasMedia ? `<div class="product-gallery-wrapper">${galleryHTML}</div>` : ''}
-            ${hasMedia ? `<div class="gallery-counter">Swipe ➡️</div>` : ''} 
+            ${hasMedia ? `<div class="gallery-counter">Swipe pour voir la video➡️</div>` : ''} 
 
             <div class="name" style="margin-top: ${hasMedia ? '0' : '20px'}">${product.name}</div>
             <div class="farm">${product.farm}</div>
